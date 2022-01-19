2966d9ba4b25e6cd984420234743ef4c9ce7d8b3
Mike Veneziale <falciformdesign@gmail.com>
2022-01-18 21:59:56 -0600

    currently allows user to click generate password button then is prompted with length criteria then a calculation is performed to get password length displayed
    git status

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,25 @@
 // Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+// generate random number of characters
+let randomNumber = function(min, lengthMax) {
+  // let value = Math.floor(Math.random() * (lengthMax - min) + min);
+  let value = lengthMax - min;
+  return value;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+// let critera = function() {
+//   let lengthMax = window.prompt("How many characters should your password be?")
+//   return lengthMax;
+// }
 
 // Upon clicking the button, prompt the user with a series of password critera
+let generatePassword = function() {
+  let lengthMax = window.prompt("How many characters should your password be?")
+ let options = randomNumber(8, lengthMax);
+
+ return options;
+
+}
+// password should equal number of characters user chooses
 
 // User inputs which criteria to include in the password
 
@@ -27,7 +29,27 @@ generateBtn.addEventListener("click", writePassword);
 
 // upon answering prompt, validate that at least one character type is selected
 
+
 // once all prompts finish, generate password that matches the selected criteria
 
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
 // when password is generated, write password to input box
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Runs the writePassword function once clicked
+generateBtn.addEventListener("click", writePassword);
 
