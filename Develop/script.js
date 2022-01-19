6915b6622525c124c5202ee28313d17ddd7b5bf4
Mike Veneziale <falciformdesign@gmail.com>
2022-01-18 19:11:40 -0600

    initial setup

--- Develop/script.js
+++ Develop/script.js
@@ -15,3 +15,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Upon clicking the button, prompt the user with a series of password critera
+
+// User inputs which criteria to include in the password
+
+// When length is prompted, user inputs and validates a length of 8-128 characters
+
+// When character type is prompted, list out options for lowercase, uppercase, numeric, and or special chars
+
+// upon answering prompt, validate that at least one character type is selected
+
+// once all prompts finish, generate password that matches the selected criteria
+
+// when password is generated, write password to input box
+
