7582eed8588216f48d844bc143484511ea36d2e1
Mike Veneziale <falciformdesign@gmail.com>
2022-01-20 21:02:40 -0600

    needs rework of randomness

--- Develop/script.js
+++ Develop/script.js
@@ -27,37 +27,68 @@ const capLetters = [
   'Y',
   'Z'
 ];
+
+const specialChars = [
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '&',
+  '*',
+  '?',
+  '~',
+]
 // generate random number of characters by taking user input number, then returning the index of a random number
-let randomNumber = function(lengthMax) {
+let randomNumber = function(array) {
   
     let value = Math.floor(Math.random() * 26);
-    
-    console.log(capLetters[value]);
-    return capLetters[value];
-  
-  
 
-
-  
+    return array[value]; 
 }
 
-// let critera = function() {
-//   let lengthMax = window.prompt("How many characters should your password be?")
-//   return lengthMax;
-// }
+// generate a random number then take that random number and assign it to the index of specialChars array
+let randomSpecChar = function() {
+  let specialValue = Math.floor(Math.random() * 9);
+
+  console.log(specialChars[specialValue]);
+  return specialChars[specialValue];
+}
+
+// prompt questions
+let promptQuestions = function() {
+  let lengthMax = window.prompt("How many characters should your password be?")
+
+  let specCharNum = window.confirm("Include special characters?");
+
+  let passwordOptions = {
+    lengthMax: lengthMax,
+    specCharNum: specCharNum
+  };
+
+  return passwordOptions;
+
+}
 
 // Upon clicking the button, prompt the user with a series of password critera
 let generatePassword = function() {
-  let lengthMax = window.prompt("How many characters should your password be?")
+  let lengthMax = window.prompt("how many characters");
+
+  // checks to see if password length is within critera
+  if (lengthMax < 8 || lengthMax > 128) {
+    window.alert("Must be at least 8 characters");
+    return generatePassword(); 
+  }
   // initialize the empty string to be appended to
   let string = "";
 
   // loop through the capital letters for as long as the user indicated then append those obtained random letters to the string variable
   for (i = 0; i < lengthMax; i++) {
-    let options = randomNumber(lengthMax);
-    console.log(randomNumber(lengthMax));
-    string = string + options;
+    let options = randomNumber(capLetters);
+    string += options;
   }
+
+
   // returns the newly appended string variable so generatePassword() equals the mix of capital letters
  return string;
 
